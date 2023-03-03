d5ccdfccc1fec9a071bd3600da6a88abca427b11
Christian Thiel <christian.thiel@gec.io>
2023-03-03 14:16:37 +0100

    add javadoc

--- runtime/src/main/java/io/quarkiverse/asyncapi/annotation/scanner/Implementation.java
+++ runtime/src/main/java/io/quarkiverse/asyncapi/annotation/scanner/Implementation.java
@@ -6,6 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Helper to describe messages. If the message-Object is type-safe, the Annotation-Scanner can obtain the
+ * message-description via reflection. But if the message is send as String (e.g. JSON-encoded), this is no possible.
  *
  * @author christiant
  */
@@ -13,5 +15,11 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Implementation {
 
+    /**
+     * Array of classes, contained in the message. E.g. if a Message List&lt;A&lt;B&gt;&gt; can be described as
+     * <code>@Implementation({List.class, A.class, B.class})</code>
+     *
+     * @return
+     */
     Class[] value();
 }
